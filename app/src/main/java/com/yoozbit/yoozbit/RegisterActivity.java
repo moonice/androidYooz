f3c27ee56a41b423deab10a1cb8f6fcc607bb9dd
alireza panahi <masiha_panahi@yahoo.com>
2019-11-08 13:51:13 +0330

    validations2

--- app/src/main/java/com/yoozbit/yoozbit/RegisterActivity.java
+++ app/src/main/java/com/yoozbit/yoozbit/RegisterActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +16,7 @@ import com.google.android.material.textfield.TextInputEditText;
 public class RegisterActivity extends AppCompatActivity {
 
     TextInputEditText name,email,mobile,pass,pass2,code;
+    Button register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,12 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         setupViews();
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkDataRegister();
+            }
+        });
     }
 
     private void setupViews() {
@@ -30,56 +40,48 @@ public class RegisterActivity extends AppCompatActivity {
         pass = (TextInputEditText) findViewById(R.id.register_pass);
         pass2 = (TextInputEditText) findViewById(R.id.register_pass2);
         code = (TextInputEditText)findViewById(R.id.register_code);
+        register_btn=(Button)findViewById(R.id.register_btn);
+    }//casting
+
+
+
+    boolean isEmail(TextInputEditText emailText){
+        CharSequence email= emailText.getText().toString();
+        return (!TextUtils.isEmpty(email)&&Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
+    boolean isEmpty(TextInputEditText text){
+        CharSequence str=text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
 
-
-
-    public void setName(){
-
-        name.getText().toString();
-        if (name == null) {
-            Toast.makeText(getApplicationContext(), "لطفا نام کاربری را وارد نمایید", Toast.LENGTH_SHORT).show();
-            return;
+    private void checkDataRegister(){
+        if (isEmpty(name)) {
+            name.setError("لطفا نام کاربری را وارد نمایید");
         }else if (name.length()<6){
-            Toast.makeText(getApplicationContext(),"نام کاربری باید حداقل 6 حرف باشد",Toast.LENGTH_SHORT).show();
-            return;
+            name.setError("نام کاربری باید حداقل 6 حرف باشد");
         }
-    }
 
-    public void setEmail(){
-
-        email.getText().toString();
-        return;
-    }
-
-    public void setMobile() {
-        mobile.getText().toString();
-        if (mobile == null || mobile.length() < 11) {
-            Toast.makeText(getApplicationContext(), "شماره موبایل وارد شده نادرست است", Toast.LENGTH_SHORT).show();
-            return;
+        if (isEmail(email)==false){
+            email.setError("لطفا یک ایمیل معتبر وارد کنید");
         }
-    }
 
-    public void setPass() {
-        pass.getText().toString();
-        if (pass == null) {
-            Toast.makeText(getApplicationContext(), "لطفا کلمه عبور را وارد نمایید", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (pass.length() < 5) {
-            Toast.makeText(getApplicationContext(), "کلمه عبور باید حداقل 5 حرف باشد", Toast.LENGTH_SHORT).show();
+        if (isEmpty(mobile)){
+            mobile.setError("لطفا شماره موبایل را وارد نمایید");
+        }else if (mobile.length()<11){
+            mobile.setError("شماره موبایل وارد شده نادرست است");
         }
-        return;
-    }
 
-    public void setPass2() {
-        pass2.getText().toString();
-        if (pass2 == null) {
-            Toast.makeText(getApplicationContext(), "کلمه عبور را تکرار کنید", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (pass2 != pass) {
-            Toast.makeText(getApplicationContext(), "کلمه عبور را عینا تکرار کنید", Toast.LENGTH_SHORT).show();
-            return;
+        if (isEmpty(pass)){
+            pass.setError("لطفا کلمه عبور را وارد نمایید");
+        }else if (pass.length()<8){
+            pass.setError("کلمه عبور باید حداقل 8 حرف باشد");
+        }
+
+        if (isEmpty(pass2)){
+            pass2.setError("کلمه عبور را تکرار کنید");
+        }else if (pass2!=pass){
+            pass2.setError("کلمه عبور را عینا تکرار کنید");
         }
 
     }
